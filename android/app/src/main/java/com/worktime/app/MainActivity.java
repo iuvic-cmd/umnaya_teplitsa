8bddbe39cabc956f505e5a0de87414823fbd458c
iuvic-cmd <iuvic1982@gmail.com>
2026-04-18 10:34:34 +0300

    Restore MainActivity

--- android/app/src/main/java/com/worktime/app/MainActivity.java
+++ android/app/src/main/java/com/worktime/app/MainActivity.java
@@ -1,17 +1,3 @@
 package com.worktime.app;
-
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        WebView webView = getBridge().getWebView();
-        WebSettings settings = webView.getSettings();
-        settings.setAllowFileAccessFromFileURLs(true);
-        settings.setAllowUniversalAccessFromFileURLs(true);
-    }
-}
+public class MainActivity extends BridgeActivity {}
